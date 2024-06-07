6031c2e015fe603edddf32e31d39fe3eec081d47
Truong Ngo <truong-ngo@soarig.vn>
2024-06-07 16:50:56 +0700

    Test popup show demo

---
+++ electron/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile('popup.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
